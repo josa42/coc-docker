3d2fe3bf4ac0df833849b5d5fc6fbecf30d74b94
Josa Gesell <josa@gesell.me>
2019-04-26 16:41:43 +0200

    Cleanp up a little

--- src/extension.ts
+++ src/extension.ts
@@ -2,47 +2,32 @@ import path from 'path'
 import { TransportKind, ExtensionContext, LanguageClient, ServerOptions, commands, workspace, services, LanguageClientOptions } from 'coc.nvim'
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  let { subscriptions } = context
+
   const config = workspace.getConfiguration().get('docker', {}) as any
-  const enable = config.enable
-  if (enable === false) return
+  if (config.enable === false) {
+    return
+  }
 
   let serverModule = context.asAbsolutePath(path.join('node_modules', 'dockerfile-language-server-nodejs', 'lib', 'server.js'));
 
   let serverOptions: ServerOptions = {
-    run: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      args: ["--node-ipc"]
-    },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: { execArgv: ["--nolazy", "--inspect=6009"] }
-    }
+    module: serverModule,
+    transport: TransportKind.ipc,
+    args: ["--node-ipc"]
   };
 
   let clientOptions: LanguageClientOptions = {
-    documentSelector: ['Dockerfile'],
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc")
-    }
+    documentSelector: ['Dockerfile']
   };
 
-  const client = new LanguageClient(
-    "dockerfile-langserver",
-    "Dockerfile Language Server",
-    serverOptions,
-    clientOptions
-  );
+  const client = new LanguageClient("docker", "dockerfile-language-server-nodejs", serverOptions, clientOptions);
 
-  subscriptions.push(
-    services.registLanguageClient(client)
+  context.subscriptions.push(
+    services.registLanguageClient(client),
+    commands.registerCommand("docker.version", async () => {
+      const v = require(path.resolve(__dirname, '..', 'package.json')).version
+      workspace.showMessage(`Version: ${v}`, 'more')
+    })
   )
-
-  subscriptions.push(commands.registerCommand("docker.version", async () => {
-    const v = require(path.resolve(__dirname, '..', 'package.json')).version
-    workspace.showMessage(`Version: ${v}`, 'more')
-  }))
 }
 
